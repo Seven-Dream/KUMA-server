7711ed0c06aa62731f8e2700c320135ff37f57a2
makura46 <makura6706@gmail.com>
2018-12-10 18:08:56 +0900

    add: define lecture tables

--- models/tableDefine.go
+++ models/tableDefine.go
@@ -5,3 +5,24 @@ type User struct {
 	Id	string	`gorm:"type:varchar(64);primary_key;not null"`
 	Password	string	`gorm:"type:char(64);not null"`
 }
+
+type Lecture struct {
+	Id	int
+	LectureName	string
+	Teachar	string
+	ClassRoom	string
+	Year	int
+	Quarter	int
+	Weeks	[]Week
+	Times	[]Time
+}
+
+type Week struct {
+	LectureID	int
+	Week	string
+}
+
+type Time struct {
+	LectureID	int
+	Time	int
+}
