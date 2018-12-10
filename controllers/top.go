7c1d532296fb3dee038555c282bcf2c0d7f8246b
makura46 <makura6706@gmail.com>
2018-12-10 21:19:55 +0900

    update: require package fmt and change: redirect to /login from /

--- controllers/top.go
+++ controllers/top.go
@@ -2,13 +2,14 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func TopGet(c *gin.Context) {
 	// session情報の取得
 	id, err := sessionCheck(c)
 	if err != nil {
-		c.Redirect(http.StatusFound, "/")
+		c.Redirect(http.StatusFound, "/login")
 		return
 	}
 
