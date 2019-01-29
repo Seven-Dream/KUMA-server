41470de0db22bfc5461a2b1ac93120d04b496d90
makura46 <makura6706@gmail.com>
2019-01-29 13:34:03 +0900

    update: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+sudo yum update -y
+sudo yum install -y docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
 export PATH=$PATH:/usr/local/bin
-
+echo "export PATH=$PATH:/usr/local/bin" >> ~/.bashrc
 sudo chmod +x /usr/local/bin/docker-compose
+sudo service docker start
