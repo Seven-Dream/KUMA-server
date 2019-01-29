a4b44cb88608986b9dfb7b03a19803aa818e2e30
makura46 <makura6706@gmail.com>
2019-01-29 14:25:47 +0900

    add: description

--- setup.sh
+++ setup.sh
@@ -9,11 +9,11 @@ echo "----- finish install docker ------"
 echo "----- start install docker compose ------"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 echo "----- finish install docker compose ------"
-echo "----- add PATH ------"
-export PATH=$PATH:/usr/local/bin
-echo "export PATH=$PATH:/usr/local/bin" >> ~/.bashrc
-echo "----- finish PATH ------"
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "----- start docker service ------"
 sudo service docker start
+
+echo "if you want to use KUMA-server,"
+echo "export PATH=$PATH:/usr/local/bin"
+echo "docker-compose up" 
