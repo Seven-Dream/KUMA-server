025cff6640abd68243e88668ae5f576a4ae0edca
makura46 <makura6706@gmail.com>
2019-01-29 13:58:32 +0900

    add: show description

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+echo "----- start yum update ------"
 sudo yum update -y
+echo "----- finish yum update ------"
+echo "----- start install docker ------"
 sudo yum install -y docker
+echo "----- finish install docker ------"
+echo "----- start install docker compose ------"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+echo "----- finish install docker compose ------"
+echo "----- add PATH ------"
 export PATH=$PATH:/usr/local/bin
 echo "export PATH=$PATH:/usr/local/bin" >> ~/.bashrc
+echo "----- finish PATH ------"
 sudo chmod +x /usr/local/bin/docker-compose
+
+echo "----- start docker service ------"
 sudo service docker start
